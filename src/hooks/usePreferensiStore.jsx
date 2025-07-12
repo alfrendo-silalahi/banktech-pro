0134eeb0f5978a1e6087ab90685c02ad588d86f4
Alfito Bramoda <alfitoprofessional@gmail.com>
2025-07-12 14:24:55 +0700

    Table Transaction History

---
+++ src/hooks/usePreferensiStore.jsx
@@ -0,0 +1,30 @@
+// src/store.js
+import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware';
+
+const usePreferensiStore = create(
+    // Gunakan middleware 'persist' dari Zustand
+    persist(
+        (set) => ({
+        // State awal
+        itemsPerPage: 10,
+        filterTipe: 'Semua', // Opsi: 'Semua', 'Transfer', 'Income'
+        isModalOpen: false,
+
+        // Actions untuk mengubah state
+        setPreferences: (prefs) => set((state) => ({ ...state, ...prefs })),
+        toggleModal: () => set((state) => ({ isModalOpen: !state.isModalOpen })),
+        }),
+        {
+        name: 'preferensi-tabel-pengguna', // Nama key di LocalStorage
+        storage: createJSONStorage(() => localStorage), // Spesifikasikan localStorage
+        partialize: (state) => ({
+            // Hanya simpan state 'itemsPerPage' dan 'filterTipe'
+            itemsPerPage: state.itemsPerPage,
+            filterTipe: state.filterTipe,
+        }),
+        }
+    )
+);
+
+export default usePreferensiStore;
